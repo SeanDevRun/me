3f2976622d8c945450717c8fc2d3db0c735c6f62
Sean Harding <SeanHarding@micornav.co.uk>
2024-09-20 14:50:51 +0100

    Configured for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  
+  // Add the publicPath for GitHub Pages
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/me/'  // Replace with your GitHub repository name
+    : '/'
 })
