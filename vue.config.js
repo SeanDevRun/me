bc9fdf48cb10cbe7e4f8d49cf8261b162f207088
Sean Harding <SeanHarding@micornav.co.uk>
2024-10-01 21:37:15 +0100

    Part 2 of aliasing component parts

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), 
+        '@assets':  path.resolve(__dirname, 'src/assets'),  // Assuming 'assets' is inside 'src'
+        '@common': path.resolve(__dirname, 'src/components/common'),  // Assuming 'assets' is inside 'src'        
+        '@components': path.resolve(__dirname, 'src/components'),  // Assuming 'assets' is inside 'src'        
+        '@views': path.resolve(__dirname, 'src/views')  // Assuming 'views' is inside 'src'
+      }
+    },
+  },
+  transpileDependencies: true,  
   // Add the publicPath for GitHub Pages
   publicPath: process.env.NODE_ENV === 'production'
     ? '/me/'  // Replace with your GitHub repository name
